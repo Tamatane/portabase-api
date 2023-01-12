575d73b86d30193e8533c9d80de4fcf4a20311b1
Tom van Hateren <tom.vanhateren@kidskonnect.nl>
2023-01-12 17:00:41 +0100

    Replace anluto/curl with Guzzle

--- src/Client.php
+++ src/Client.php
@@ -2,9 +2,14 @@
 
 namespace AtaneNL\PortaBase;
 
-use anlutro\cURL\cURL;
-use anlutro\cURL\Request;
+use AtaneNL\PortaBase\Exceptions\InvalidParameterException;
+use AtaneNL\PortaBase\Exceptions\InvalidRequestException;
+use AtaneNL\PortaBase\Exceptions\RemoteException;
+use AtaneNL\PortaBase\Exceptions\UnauthorizedException;
 use CURLFile;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Utils;
 
 class Client
 {
@@ -15,9 +20,9 @@ class Client
     const QUALIFICATION_VOG = 'vog';
     const QUALIFICATION_RIE = 'rie';
 
-    private $curl;
-    private $portabase_url;
-    private $portabase_key;
+    private \GuzzleHttp\Client $client;
+    private string $portabase_url;
+    private string $portabase_key;
 
     /**
      * Initialize a PortaBase Client
@@ -25,77 +30,79 @@ class Client
      * @param string $portabase_url PortaBase protocol and domain, e.g. https://awesome.portabase.nl
      * @param string $portabase_key PortaBase API key
      */
-    public function __construct($portabase_url, $portabase_key)
+    public function __construct(string $portabase_url, string $portabase_key)
     {
         $this->portabase_url = $portabase_url;
         $this->portabase_key = $portabase_key;
-        $this->curl = new cURL();
-        $this->curl->setDefaultHeaders([
-            'api-key' => $portabase_key
-            ]);
+        $this->client = new \GuzzleHttp\Client([
+            'http_errors' => false,
+            'headers' => [
+                'accept' => 'application/json',
+                'api-key' => $portabase_key,
+            ]
+        ]);
     }
 
     /**
      * Retrieve all host parents
-     *
-     * @throws \anlutro\cURL\cURLException Thrown by the underlying cURL library
-     * @throws Exceptions\RemoteException Thrown if the response was not a 200 OK
-     *
      * @return array Array of hosts
-     */
-    public function getHosts()
-    {
-        $response = $this->curl->jsonGet("{$this->portabase_url}/api/1.0/gastouders");
-        return $this->parseResponse($response);
-    }
-
-    /**
-     * @param $hostId
-     * @return array Array with one host
-     * @throws Exceptions\InvalidParameterException
+     * @throws GuzzleException Thrown by the underlying guzzle client
      * @throws Exceptions\InvalidRequestException
      * @throws Exceptions\RemoteException
      * @throws Exceptions\UnauthorizedException
      */
-    public function getHost($hostId)
+    public function getHosts(): array
     {
-        if (!is_numeric($hostId)) {
-            throw new Exceptions\InvalidParameterException('Invalid host ID');
-        }
-        $response = $this->curl->jsonGet("{$this->portabase_url}/api/1.0/gastouders/${hostId}");
+        $response = $this->client->get("{$this->portabase_url}/api/1.0/gastouders");
+        return $this->parseResponse($response);
+    }
+
+    /**
+     * @param int $hostId
+     * @return array Array with one host
+     * @throws GuzzleException Thrown by the underlying guzzle client
+     * @throws InvalidRequestException
+     * @throws RemoteException
+     * @throws UnauthorizedException
+     */
+    public function getHost(int $hostId): array
+    {
+        $response = $this->client->get("{$this->portabase_url}/api/1.0/gastouders/${hostId}");
         return $this->parseResponse($response);
     }
 
     /**
      * Retrieve all managers
-     *
-     * @throws \anlutro\cURL\cURLException Thrown by the underlying cURL library
-     * @throws Exceptions\RemoteException Thrown if the response was not a 200 OK
+     * @return array Array of managers
+     * @throws GuzzleException
+     * @throws InvalidRequestException
+     * @throws RemoteException
+     * @throws UnauthorizedException
      */
-    public function getManagers()
+    public function getManagers(): array
     {
-        $response = $this->curl->jsonGet("{$this->portabase_url}/api/1.0/managers");
+        $response = $this->client->get("{$this->portabase_url}/api/1.0/managers");
         return $this->parseResponse($response);
     }
 
     /**
-     * @param $hostId
-     * @param $date
-     * @param $expireDate
-     * @param $type
-     * @param CURLFile $attachment
-     * @param null $comments
-     * @param null $lrkp
-     * @param null $actionPlanApprovalDate
-     * @param CURLFile|null $actionPlan
+     * @param int $hostId Id of host in Portabase
+     * @param string $date Receive date of qualification formatted as YYYY-MM-DD
+     * @param string $expireDate Expiry date of qualification formatted as YYYY-MM-DD
+     * @param string $type One of the defined qualification types
+     * @param CURLFile $attachment Attachment file
+     * @param string|null $comments
+     * @param string|null $lrkp
+     * @param string|null $actionPlanApprovalDate Approval date formatted as YYYY-MM-DD
+     * @param CURLFile|null $actionPlan Null or a file containing the action plan
      * @param bool $actionPlanExecuted
-     * @return mixed
-     * @throws Exceptions\InvalidParameterException
-     * @throws Exceptions\InvalidRequestException
-     * @throws Exceptions\RemoteException
-     * @throws Exceptions\UnauthorizedException
+     * @throws GuzzleException
+     * @throws InvalidParameterException
+     * @throws InvalidRequestException
+     * @throws RemoteException
+     * @throws UnauthorizedException
      */
-    public function postQualification($hostId, $date, $expireDate, $type, CURLFile $attachment, $comments = null, $lrkp = null, $actionPlanApprovalDate = null, CURLFile $actionPlan = null, $actionPlanExecuted = false)
+    public function postQualification(int $hostId, string $date, string $expireDate, string $type, CURLFile $attachment, string $comments = null, string $lrkp = null, string $actionPlanApprovalDate = null, CURLFile $actionPlan = null, bool $actionPlanExecuted = false): array
     {
         if (!in_array($type, [self::QUALIFICATION_FIRSTAID, self::QUALIFICATION_PASSPORT, self::QUALIFICATION_IDCARD, self::QUALIFICATION_DRIVERSLICENSE, self::QUALIFICATION_VOG, self::QUALIFICATION_RIE])) {
             throw new Exceptions\InvalidParameterException('Invalid type');
@@ -104,45 +111,65 @@ class Client
             throw new Exceptions\InvalidParameterException('Missing parameter lrkp');
         }
 
-        $params = [
-            'gastouderId' => $hostId,
-            'kwalificatieType' => $type,
-            'datumAfgifte' => $date,
-            'verloopDatum' => $expireDate,
-            'bijlage1' => $attachment
+        $multipart = [
+            ['name' => 'gastouderId', 'contents' => $hostId],
+            ['name' => 'kwalificatieType', 'contents' => $type],
+            ['name' => 'datumAfgifte', 'contents' => $date],
+            ['name' => 'verloopDatum', 'contents' => $expireDate],
+            [
+                'name' => 'bijlage1',
+                'contents' => Utils::tryFopen($attachment->getFilename(), 'r'),
+                'filename' => $attachment->getPostFilename(),
+                'headers' => ['content-type' => $attachment->getMimeType()],
+            ],
         ];
+
         if ($type == self::QUALIFICATION_RIE) {
-            $params['lrkpNummer'] = $lrkp;
+            $multipart[] = ['name' => 'lrkpNummer', 'contents' => $lrkp];
         }
         if (!is_null($actionPlanApprovalDate)) {
-            $params['datumAkkoordActieplan'] = $actionPlanApprovalDate;
+            $multipart[] = ['name' => 'datumAkkoordActieplan', 'contents' => $actionPlanApprovalDate];
         }
         if (!is_null($actionPlan)) {
-            $params['actieplan'] = 1;
-            $params['bijlage2'] = $actionPlan;
+            $multipart[] = ['name' => 'actieplan', 'contents' => 1];
+            $multipart[] = [
+                'name' => 'bijlage2',
+                'contents' => Utils::tryFopen($actionPlan->getFilename(), 'r'),
+                'filename' => $actionPlan->getPostFilename(),
+                'headers' => ['content-type' => $actionPlan->getMimeType()],
+            ];
             if ($actionPlanExecuted) {
-                $params['actieplanUitgevoerd'] = 1;
+                $multipart[] = ['name' => 'actieplanUitgevoerd', 'contents' => 1];
             }
         }
         if (!is_null($comments)) {
-            $params['opmerkingen'] = $comments;
+            $multipart[] = ['name' => 'opmerkingen', 'contents' => $comments];
         }
 
-        $request = $this->curl->newRequest('POST', "{$this->portabase_url}/api/1.0/kwalificatie", $params, Request::ENCODING_RAW);
-        $response = $request->send();
+        $response = $this->client->post("{$this->portabase_url}/api/1.0/kwalificatie", [
+            'multipart' => $multipart,
+        ]);
+
         return $this->parseResponse($response);
     }
 
-    private function parseResponse($response)
+    /**
+     * @throws InvalidRequestException Thrown if the request is deemed invalid by the server
+     * @throws UnauthorizedException Thrown if we are not allowed to call an api endpoint
+     * @throws RemoteException Thrown for other non-200 status codes
+     */
+    private function parseResponse(Response $response): array
     {
-        if ($response->statusCode == 200 || $response->statusCode == 201) {
-            return json_decode($response->body);
-        } elseif ($response->statusCode == 400) {
-            throw new Exceptions\InvalidRequestException($response->body, $response->statusCode);
-        } elseif ($response->statusCode == 403) {
-            throw new Exceptions\UnauthorizedException($response->body, $response->statusCode);
+        $statusCode = $response->getStatusCode();
+        $body = $response->getBody();
+        if ($statusCode == 200 || $statusCode == 201) {
+            return (array)json_decode($body);
+        } elseif ($statusCode == 400) {
+            throw new Exceptions\InvalidRequestException($body, $statusCode);
+        } elseif ($statusCode == 401) {
+            throw new Exceptions\UnauthorizedException($body, $statusCode);
         } else {
-            throw new Exceptions\RemoteException($response->body, $response->statusCode);
+            throw new Exceptions\RemoteException($body, $statusCode);
         }
     }
 }
